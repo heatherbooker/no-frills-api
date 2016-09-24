b158688e5e1292a8f3e4ec2f94d52e34b11aef7f
Heather Booker <Heather Booker>
2016-09-24 16:44:14 -0700

    export scraper instead of using constructor

--- src/Scraper.js
+++ src/Scraper.js
@@ -3,7 +3,7 @@ const phantomBin = 'phantomjs';
 const path = require('path');
 const pathToScraper = path.join(__dirname, './phantom/phantomScraper.js');
 
-function phantomResults(phantom) {
+function getPhantomResults(phantom) {
   if (phantom.error) {
     if (phantom.error.path === phantomBin) {
       return 'Error - are you sure you have phantomjs installed?';
@@ -13,16 +13,13 @@ function phantomResults(phantom) {
   return phantom.stdout;
 }
 
-function Scraper() {
-  function scrape(extractors) {
-    // Synchronously runs phantomjs scraper in a node child process.
-    const phantom = spawnSync(phantomBin, [pathToScraper], {encoding: 'utf8'});
+function scrape(extractors) {
+  // Synchronously runs phantomjs scraper in a node child process.
+  const phantom = spawnSync(phantomBin, [pathToScraper], {encoding: 'utf8'});
 
-    return phantomResults(phantom);
-  }
-  return {scrape: scrape};
+  return getPhantomResults(phantom);
 }
 
-var scraper = new Scraper();
-var data = scraper.scrape();
-console.log('after scraping:', data);
+module.exports = {
+  scrape
+}
