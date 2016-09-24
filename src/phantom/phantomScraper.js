907d480e8d60a774048b7ec5ceff52ed02b83e3b
Heather Booker <Heather Booker>
2016-09-24 15:42:39 -0700

    use path.join

--- src/phantom/phantomScraper.js
+++ src/phantom/phantomScraper.js
@@ -8,32 +8,25 @@ page.onConsoleMessage = function(message) {
   });
 };
 
-
 page.open('http://www.nofrills.ca/en_CA/flyers.banner@NOFR.storenum@3410.week@current.html', function(status) {
-
   if (status === 'success') {
-    console.log('success');
-
     waitFor(function() {
       return page.evaluate(function() {
         return document.querySelector('.card-grid-layout div.card');
       });
-
     }, function() {
-
       page.evaluate(function() {
         var products = [];
         var cards = document.querySelectorAll('.card-grid-layout div.card');
-        
-        for (var i = 0; i < 9; i ++) {
+
+        for (var i = 0; i < 9; i++) {
           products.push(cards[i].textContent);
           console.log(i + 'th card: ' + cards[i].textContent);
         }
       });
-
       phantom.exit();
-
     });
+  } else {
+    console.log('error opening webpage:', status);
   }
-
 });
