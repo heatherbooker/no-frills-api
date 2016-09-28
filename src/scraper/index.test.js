f01becea2e932e73d70cd88f0016ac5b43bce098
Heather Booker <Heather Booker>
2016-09-27 23:24:58 -0700

    improve index.js test for scraper

--- src/scraper/index.test.js
+++ src/scraper/index.test.js
@@ -1,11 +1,24 @@
 var assert = require('chai').assert;
 var scraper = require('./index.js');
+var path = require('path');
 
+var fileData;
+
+var destinationFile = path.join(__dirname, '../data/no_frills_products.json');
 
 describe('scraper', function() {
 
-  it('should be an object', function() {
-    assert.typeOf(scraper, 'object', 'scraper is an object');
+  it('should write a file, ' + destinationFile, function() {
+    // It takes a while to fetch the data from the website.
+    this.timeout(5000);
+    scraper.scrape();
+    fileData = require(destinationFile);
+    assert.isOk(fileData);
+  });
+
+  it('should write an object to the file, with array of products', function() {
+    assert.isObject(fileData);
+    assert.isArray(fileData.products);
   });
 
 });
