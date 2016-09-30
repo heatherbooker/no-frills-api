6fce84dfc3e09cb484e1dc172f507d7fcda7ef9f
Heather Booker <Heather Booker>
2016-09-30 12:53:30 -0700

    comment

--- src/scraper/extractors.js
+++ src/scraper/extractors.js
@@ -1,26 +1,44 @@
+/**
+ * @file Extractors are used by the scraper to format the product objects.
+ */
 function extractProducts(arrayOfProducts) {
+
+  // If there's more than one argument, it may be because the
+  // objects should have been aggregated into an array.
   if (arguments.length > 1) {
     throw new Error('Error extracting products: too many arguments!');
   }
+
   return arrayOfProducts.map(product => {
+
     product.priceSavings = product.priceSavings.replace('SAVE ', '');
+
+    // Removes HTML tags found in product.description.
     product.description = product.description
       .replace(/<.*?>/g, '');
+
+    // Product description in French is last part of description, after 2 '\n'.
     var descriptions = product.description.split('/\n\n');
     product.description = descriptions[0].trim();
     product.french = descriptions[1];
+
     return removeNullProperties(product);
+
   });
 }
 
 function removeNullProperties(product) {
+
+  // These properties are sometimes or always set to null or 'n/a'.
   var nullProperties = ['points', 'rank', 'itemDetailsUrl', 'flyerUrl',
                         'correctionNotice', 'priceSavings'];
+
   nullProperties.forEach(nullProp => {
     if (!product[nullProp] || product[nullProp] === 'n/a') {
       delete product[nullProp];
     }
   });
+
   return product;
 }
 
--- src/scraper/index.js
+++ src/scraper/index.js
@@ -1,3 +1,6 @@
+/**
+ * @file Makes http request to nofrills, writes formatted data to another file.
+ */
 var fs = require('fs');
 var request = require('request');
 var extract = require('./extractors.js');
@@ -17,6 +20,7 @@ var headers = {
   'Cookie': `JSESSIONID=0001VjSBD8ZOhtDgdDYldX_I9pW:-H88BM; storeIdCookie=292; storeNumCookie=784; storeProvienceCookie=ON; SessionPersistence-publish=CLIENTCONTEXT%3A%3DvisitorId%3D%2CvisitorId_xss%3D%7CPROFILEDATA%3A%3D%7C; cq5pr=CQ5_HER; NSC_qsdrif.mpcmbx.db_80=ffffffff0960541945525d5f4f58455e445a4a423660`
 };
 
+// If we don't know how many products there are, there's probably at least 1.
 function getOptions(numOfProducts = 1) {
   return {
     url: `http://www.nofrills.ca/banners/publication/v1/en_CA/NOFR/current/784/items?start=0&rows=${numOfProducts}&tag=`,
@@ -27,6 +31,7 @@ function getOptions(numOfProducts = 1) {
 
 function scrape() {
 
+  // fs.write won't work unless 'data' dir exists.
   fs.stat('src/data', function(err, stats) {
     if (!stats) {
       fs.mkdir('src/data');
@@ -38,29 +43,39 @@ function scrape() {
   var filePath = 'src/data/no_frills_products.json';
 
   return new Promise(function(resolve, reject) {
+
+    // First we need to check how many products there are for this flyer.
     request(getOptions(), function(err, response, body) {
+
       if (!err && response.statusCode === 200) {
         var numOfProducts = JSON.parse(body).flyerResponse.numFound;
+
+        // Then we can fetch that number of products.
         request(getOptions(numOfProducts), function(err, response, body) {
 
           if (!err && response.statusCode === 200) {
             var productList = JSON.parse(body).flyerResponse.docs;
             var products = extract.products(productList);
             resolve(products);
+
           } else {
             throw new Error(`secondary request to nofrills endpoint failed: ${err}`);
           }
         });
+
       } else {
         throw new Error('primary request to nofrills endpoint failed: ' + err);
       }
+
     });
-  }).then(function(products) {
+  })
+  .then(function(products) {
+
     var fileContents = JSON.stringify({products}, null, 2);
     fs.writeFile(filePath, fileContents);
+
     return filePath;
   });
-
 }
 
 module.exports = {
