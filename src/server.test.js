225c7773c472b120a3763e13d106610bf7e0be47
Heather Booker <Heather Booker>
2016-10-03 10:59:06 -0700

    fix lint errs

--- src/server.test.js
+++ src/server.test.js
@@ -5,12 +5,12 @@ var should = chai.should();
 var server = require('./server.js');
 
 
-
 describe.only('server', function() {
   it('should respond with "Hello World!" to home route', function(done) {
     chai.request(server)
       .get('/')
       .end(function(err, res) {
+        should.not.exist(err);
         res.should.have.status(200);
         res.should.have.property('text');
         res.text.should.equal('Hello World!');
