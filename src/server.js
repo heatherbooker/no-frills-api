7a6eab0116d4f128a715a259d3ea31e738d12cc1
Heather Booker <Heather Booker>
2016-10-05 12:43:51 -0700

    put scraping in server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var server = express();
+const express = require('express');
+const server = express();
+const scraper = require('./scraper');
+
 
 server.get('/', (req, res) => {
-  res.send('Hello World!');
+  scraper.scrape()
+    .then(data => {
+      res.send('Scraping successful;<br /><br />store:<br /><br /><br />' +
+                JSON.stringify(data.store, null, '<br />') +
+                '<br /><br /><br />first product:<br /><br /><br />' +
+                JSON.stringify(data.flyer[0], null, '<br />'));
+    })
+    .catch(error => {
+      res.send('Error while attempting to scrape: \n', error);
+    });
 });
 
-server.listen(3000, () => {
-  console.log(`Wee baby "Hello World" server listening on port 3000!`);
+server.listen(8080, () => {
+  console.log(`Go to http://localhost:8080`);
 });
 
 module.exports = server;
