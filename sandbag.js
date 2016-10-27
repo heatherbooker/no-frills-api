f2b48689ac2fb2d0fed753441405b41b8ac4ba39
Heather Booker <Heather Booker>
2016-10-26 19:04:58 -0700

    put city and province extractors in their own files

--- sandbag.js
+++ sandbag.js
@@ -1,49 +1,11 @@
 const request = require('request');
-const cheerio = require('cheerio');
-const storeExtractor = require('./src/scraper/extractors/storeExtractor.js');
+const extractor = require('./src/scraper/extractors/index.js');
 
 const nofrillsData = {};
 
-function extractProvinces(data, nofrillsData) {
-  const provinces = JSON.parse(data).provincePrompt;
-  const provinceCodes = provinces.map(province => ({code: province.label}));
-  nofrillsData.provinces = provinceCodes;
-
-  return nofrillsData;
-}
-
-function extractCities(data, nofrillsData) {
-  const cities = [];
-  let currentProvince;
-  const $ = cheerio.load(data);
-
-  $('a').each(function() {
-    const link = $(this).attr('href');
-
-    // The city name is after the two char province, in (.+?) .
-    const match = link.match(/store-list-page\.([A-Z]{2})\.(.+?)\.html/);
-
-    if (match) {
-      currentProvince = match[1];
-      const cityObject = {
-        nameForEndpoint: match[2],
-        name: match[2].replace(/%20/g, ' ').trim()
-      };
-      cities.push(cityObject);
-    }
-  });
-
-  nofrillsData.provinces.forEach(province => {
-    if (province.code === currentProvince) {
-      province.cities = cities;
-    }
-  });
-
-  return nofrillsData;
-}
 
 function extractStores(data, nofrillsData) {
-  const stores = storeExtractor(data);
+  const stores = extractor.extractStores(data);
   // Finds the appropraite city in nofrillsData to add stores to.
   const provinceCode = stores[0].address.province;
   const cityName = stores[0].address.city;
@@ -59,14 +21,14 @@ const firstEndpoint = `http://www.nofrills.ca/banners/global/v1/en_CA/nofrills`;
 makeDelayedRequest(firstEndpoint)
   // Gets list of provinces.
   .then(data => {
-    return extractProvinces(data, nofrillsData);
+    return extractor.extractProvinces(data, nofrillsData);
   })
   // Gets list of cities in each province.
   .then(nofrillsData => {
     const promises = nofrillsData.provinces.map(province => {
       const cityEndpoint = `http://www.nofrills.ca/en_CA/store-list-page.${province.code}.html`;
       return makeDelayedRequest(cityEndpoint).then(data => {
-        return extractCities(data, nofrillsData);
+        return extractor.extractCities(data, nofrillsData);
       });
     });
     return Promise.all(promises);
