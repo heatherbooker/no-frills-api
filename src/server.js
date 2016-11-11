b92eec22328b7322199b89a2c57c39c3dddf222f
Heather Booker <Heather Booker>
2016-11-11 16:24:06 -0500

    add actual route for index.html

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ const flyerRouter = require('./controllers/flyerController.js');
 const storeRouter = require('./controllers/storeController.js');
 const noFrills = require('./noFrills.js');
 
+server.use(express.static('static'));
+
+server.get('/', function(req, res) {
+  res.render('index.html');
+});
 
 server.use('/api/v0/flyers', flyerRouter);
 
 server.use('/api/v0/stores', storeRouter);
-
-server.use(express.static('static'));
 
 
 noFrills.on('noFrills-initialized', () => {
